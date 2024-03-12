a9650b0db910530524f9fc87eda0d346e7b468f6
EhsanKhodadad <ehsan.khodadad@gmail.com>
2024-03-12 13:18:45 +0000

    Deploying to gh-pages from  @ bf6da9f59a092bcc0cc16fbe6d8101a0393c62ec 🚀

---
+++ navtreedata.js
@@ -0,0 +1,99 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "reactor-c", "index.html", [
+    [ "README", "d8/d7b/md_core_2federated_2_r_t_i_2_r_e_a_d_m_e.html", null ],
+    [ "hashset.c", "d7/dd5/md_core_2utils_2hashset_2_r_e_a_d_m_e.html", [
+      [ "Example", "d7/dd5/md_core_2utils_2hashset_2_r_e_a_d_m_e.html#autotoc_md1", null ]
+    ] ],
+    [ "hashset.c", "d2/de9/md_include_2core_2utils_2hashset_2_r_e_a_d_m_e.html", [
+      [ "Example", "d2/de9/md_include_2core_2utils_2hashset_2_r_e_a_d_m_e.html#autotoc_md3", null ]
+    ] ],
+    [ "LICENSE", "d2/d35/md__l_i_c_e_n_s_e.html", null ],
+    [ "README", "d0/d05/md_low__level__platform_2_r_e_a_d_m_e.html", null ],
+    [ "README", "d3/dcc/md__r_e_a_d_m_e.html", [
+      [ "Reactor-C: A reactor runtime implementation in C", "d3/dcc/md__r_e_a_d_m_e.html#autotoc_md4", [
+        [ "Documentation", "d3/dcc/md__r_e_a_d_m_e.html#autotoc_md5", null ],
+        [ "Code-formatting", "d3/dcc/md__r_e_a_d_m_e.html#autotoc_md6", null ],
+        [ "Testing", "d3/dcc/md__r_e_a_d_m_e.html#autotoc_md7", null ]
+      ] ]
+    ] ],
+    [ "/lib/C/util/", "dc/d6e/md_util_2_r_e_a_d_m_e.html", null ],
+    [ "util/tracing", "d2/da5/md_util_2tracing_2_r_e_a_d_m_e.html", [
+      [ "Installing", "d2/da5/md_util_2tracing_2_r_e_a_d_m_e.html#autotoc_md10", null ]
+    ] ],
+    [ "Trace sequence diagram visualizer", "dd/d9b/md_util_2tracing_2visualization_2_r_e_a_d_m_e.html", [
+      [ "Installing", "dd/d9b/md_util_2tracing_2visualization_2_r_e_a_d_m_e.html#autotoc_md15", null ],
+      [ "Running", "dd/d9b/md_util_2tracing_2visualization_2_r_e_a_d_m_e.html#autotoc_md16", null ]
+    ] ],
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d2/dcd/tag_8h.html#a3cf238d5a1a842131a5adf916d571d12",
+"d5/d38/structpqueue__t.html#ab405fbb415065192ce9a1a39b5f45978",
+"d6/d68/vector_8h.html#a85cdea38a35554168aa2277d83f5a957",
+"d7/d58/sensor__simulator_8c.html#a8f5bc282c0405791a6bf0c02abe54df9a930e1fe9adfb52706ff51c4dd2833dcb",
+"d9/da6/net__common_8h.html#ae17d641bfe8dcd23091a4cfbae028660",
+"da/dfb/low__level__platform_8h.html#a9a43894d4caf7e2fc1e75b9b49d7285d",
+"dc/d69/fedsd_8py.html#ad7c9235053b4ce8b4fdda05fbe135115",
+"de/dcd/influxdb_8h.html#a00d3a4bed017647716879225efc134d4",
+"globals_func_v.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
